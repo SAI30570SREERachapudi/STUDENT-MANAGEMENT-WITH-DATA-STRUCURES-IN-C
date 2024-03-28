d128053ab241e9f238400ae8249e14afe6ad6a33
RACHAPUDISAISREE <saisreegithub@gmail.com>
2024-03-28 21:52:18 +0530

    Project Started

---
+++ code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+struct node{
+char firstName[100];
+char secondName[100]
+char courseCode[100];
+char grade;
+struct node *next;
+}*head=NULL,*ptr,*newnode;
+int main()
+{
+
+
+
+}
