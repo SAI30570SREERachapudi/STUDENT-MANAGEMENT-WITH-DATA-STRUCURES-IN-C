4a3a01e9e74a108b48226f386295135f8eabc667
RACHAPUDISAISREE <saisreegithub@gmail.com>
2024-04-04 15:03:46 +0530

    Update funtion corrected

--- code.c
+++ code.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<wchar.h>
 struct node{
 char firstName[100];
 char secondName[100];
@@ -15,6 +16,8 @@ void display();
 int main(){
 int ch;
 while(1){
+	 wchar_t emoji[] = L"\U0001F600"; // Unicode for the smiling face emoji
+    wprintf(L"Emoji: %ls\n", emoji);
         printf("========= Student Information System ========= \n");
         printf("Enter 1 to add Student Details\n");
         printf("Enter 2 to update the details of a student\n");
@@ -74,26 +77,66 @@ while(1){
     printf("Student details added successfully\n");
 }
 
-void update(){
-printf("Enter First Name of the Student to update the details\n");
-char keyFirstName[100];
-char keySecondName[100];
-scanf("%[^\n]s",keyFirstName);
-printf("Enter Second name of the Student to update the datails\n");
-scanf("%[^\n]s",keySecondName);
-ptr=head;
-while(ptr!->NULL&&strcpm(ptr->secondName,keySecondName)&&strcmp(ptr->firstName,keyFirstName)
-                {
-                ptr=ptr->next;
-                }
-        if(ptr==NULL){
-                printf("There is no such student with the name %s %s",keyFirstName,keySecondName);
-                printf("Enter 1 to search again");
-                int ch;
-                scanf("%d",&ch);
+void update() {
+    printf("Enter First Name of the Student to update the details: ");
+    char keyFirstName[100];
+    char keySecondName[100];
+    scanf(" %[^\n]", keyFirstName);
+    printf("Enter Second name of the Student to update the details: ");
+    scanf(" %[^\n]", keySecondName);
+    ptr = head;
+    while (ptr != NULL && (strcmp(ptr->secondName, keySecondName) != 0 || strcmp(ptr->firstName, keyFirstName) != 0)) {
+        ptr = ptr->next;
+    }
+    if (ptr == NULL) {
+        printf("There is no such student with the name %s %s\n", keyFirstName, keySecondName);
+        printf("Enter 1 to search again\n");
+        printf("Enter any other number to exit\n");
+        int ch;
+        scanf("%d", &ch);
+        switch (ch) {
+            case 1:
                 update();
+                break;
+            default:
+                break;
         }
+    } else {
+        printf("Enter 1 to update First Name of the student\n");
+        printf("Enter 2 to update Second Name of the student\n");
+        printf("Enter 3 to update Course Code of the student\n");
+        printf("Enter 4 to update Grade of the student\n");
+        int choice;
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("Enter First Name: ");
+                scanf(" %[^\n]", ptr->firstName);
+                printf("First Name updated successfully\n");
+                break;
+            case 2:
+                printf("Enter Second Name: ");
+                scanf(" %[^\n]", ptr->secondName);
+                printf("Second Name updated successfully\n");
+                break;
+            case 3:
+                printf("Enter Course Code: ");
+                scanf(" %[^\n]", ptr->courseCode);
+                printf("Course code updated successfully\n");
+                break;
+            case 4:
+                printf("Enter Grade: ");
+                scanf(" %c", &ptr->grade);
+                printf("Grade updated successfully\n");
+                break;
+            default:
+                printf("Invalid choice\n");
+                break;
+        }
+    }
 }
+
 void display() {
     if (head == NULL) {
           printf("No    student   details to display.\n");
